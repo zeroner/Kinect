30d91a0a02a38aa5e6f0d87ea6ada6fe9cc9474e
zeroner <xeroner.z@gmail.com>
2014-02-21 01:19:49 -0600

    Practica Controles del Toolkit

---
+++ Practica 9/ControlesDelToolkit/ControlesDelToolkit/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
+using Microsoft.Kinect.Toolkit.Controls;
+
+namespace ControlesDelToolkit
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KinectSensorChooser miKinect;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            miKinect = new KinectSensorChooser();
+            miKinect.KinectChanged += miKinect_KinectChanged;
+            sensorChooserUI.KinectSensorChooser = miKinect;
+            miKinect.Start();
+        }
+
+        void miKinect_KinectChanged(object sender, KinectChangedEventArgs e)
+        {
+            bool error = true;
+
+            if (e.OldSensor == null)
+            {
+                try
+                {
+                    e.OldSensor.DepthStream.Disable();
+                    e.OldSensor.SkeletonStream.Disable();
+                }
+                catch (Exception)
+                {
+                    error = true;
+                }
+            }
+
+            if (e.NewSensor == null)
+                return;
+
+            try
+            {
+                e.NewSensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+                e.NewSensor.SkeletonStream.Enable();
+
+                try
+                {
+                    e.NewSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+                    e.NewSensor.DepthStream.Range = DepthRange.Near;
+                    e.NewSensor.SkeletonStream.EnableTrackingInNearRange = true;
+                }
+                catch (InvalidOperationException)
+                {
+                    e.NewSensor.DepthStream.Range = DepthRange.Default;
+                    e.NewSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                }
+            }
+            catch (InvalidOperationException)
+            {
+                error = true;
+            }
+
+            ZonaCursor.KinectSensor = e.NewSensor;
+        }
+
+        private void KinectTileButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void KinectCircleButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Bien Hecho!");
+        }
+    }
+}
